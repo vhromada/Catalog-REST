84c91112215e5d2b862c1a3274fb1fa7d9fa39f5
Vladimir Hromada <vhromada@gmail.com>
2020-01-25 12:16:59 +0100

    Parent version -> 4.0.2, Catalog version -> 8.0.1, Common version -> 3.0.2

--- src/test/kotlin/cz/vhromada/catalog/rest/CatalogContextTest.kt
+++ src/test/kotlin/cz/vhromada/catalog/rest/CatalogContextTest.kt
@@ -2,8 +2,10 @@ package cz.vhromada.catalog.rest
 
 import cz.vhromada.common.test.AppContextTest
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.web.WebAppConfiguration
 
 @SpringBootTest
 @WebAppConfiguration
+@ActiveProfiles("test")
 class CatalogContextTest : AppContextTest()
